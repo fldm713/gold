222adbeff2f4c67679e8eaa4053feb197cd6c5c4
fldm713 <ma.honglin95@gmail.com>
2022-07-26 20:43:53 -0700

    A simple example

--- blog/main.go
+++ blog/main.go
@@ -12,9 +12,5 @@ func main() {
 	engine.Add("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to the golden era!\n")
 	})
-	userGroup := engine.Group("user")
-	userGroup.Add("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi user, welcome to the golden era!\n")
-	})
 	engine.Run()
 }
